3392dfbef2c9a391bb152218681aeb38a29fcf3e
Joe <joej21@gmail.com>
2017-12-06 17:49:59 -0500

    adding rspec, init, dish spec

---
+++ spec/models/dish_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Dish, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+
+# TEST HAPPY PATH
+# - NAME
+# - IMG_URL present
+
+# - NAME
+# - IMG_URL missing
+
+# TEST VALIDATIONS
+# Name missing
+
+# unique names
--- test/models/dish_test.rb
+++ test/models/dish_test.rb
@@ -1,17 +1,4 @@
 require 'test_helper'
-
 class DishTest < ActiveSupport::TestCase
-
-	# TEST HAPPY PATH
-	# - NAME
-	# - IMG_URL present
-
-	# - NAME
-	# - IMG_URL missing
-
-	# TEST VALIDATIONS
-	# Name missing
-
-	# unique names
 
 end
