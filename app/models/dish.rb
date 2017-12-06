486e150812023bbfc95d26f23939915627ae7216
Joe <joej21@gmail.com>
2017-12-06 17:26:44 -0500

    adding dish model, basic test conditions for validations

---
+++ app/models/dish.rb
@@ -0,0 +1,4 @@
+class Dish < ApplicationRecord
+	validates :name, :presence => true
+	validates :name, :uniqueness => true
+end
---
+++ test/models/dish_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class DishTest < ActiveSupport::TestCase
+
+	# TEST HAPPY PATH
+	# - NAME
+	# - IMG_URL present
+
+	# - NAME
+	# - IMG_URL missing
+
+	# TEST VALIDATIONS
+	# Name missing
+
+	# unique names
+
+end
